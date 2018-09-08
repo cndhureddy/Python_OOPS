dbfdc999bfe7e0c45c5b21ed8909504f1de232c8
cndhureddy <31669603+cndhureddy@users.noreply.github.com>
2018-09-08 18:13:55 -0400

    Create Instance_Attributes.py

---
+++ Instance_Attributes.py
@@ -0,0 +1,9 @@
+# In Dog() class, each dog has a specific name and age
+
+class Dog:
+
+    # Initializer / Instance Attributes
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        #  self variable is an instance of the class
