1b419a144344f1f80b81c0805b03eae4fb85a893
cndhureddy <31669603+cndhureddy@users.noreply.github.com>
2018-09-08 19:17:03 -0400

    Create Child_vs_parent_class.py
    Child class overriding the Functionality of a Parent Class

---
+++ Child_vs_parent_class.py
@@ -0,0 +1,19 @@
+#Overriding the Functionality of a Parent Class
+'''
+>>> class Dog:
+...     species = 'mammal'
+...
+>>> class SomeBreed(Dog):
+...     pass
+...
+>>> class SomeOtherBreed(Dog):
+...     species = 'reptile'
+...
+>>> frank = SomeBreed()
+>>> frank.species
+'mammal'
+>>> beans = SomeOtherBreed()
+>>> beans.species
+'reptile'
+
+'''
